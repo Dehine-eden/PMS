3f4b66d816b24627977e7f67d10ee0a315b5997a
Dehine-eden <119399020+Dehine-eden@users.noreply.github.com>
2025-05-19 14:05:29 +0300

    Create ProjectStatus.cs
    Project status selection options

---
+++ Backend/Model/Enums/ProjectStatus.cs
@@ -0,0 +1,13 @@
+    namespace ProjectManagementSystem1.Model.Enums
+{
+    public enum ProjectTaskStatus
+    {
+        ToDo = 1,
+        Pending = 2,
+        InProgress = 3,
+        Completed = 4,
+        OnHold = 5,
+        Cancelled = 6
+    }
+}
+
