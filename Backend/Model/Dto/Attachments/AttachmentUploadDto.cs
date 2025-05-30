c178e5e5f176df4a742456212e0ddfa40c0f19a2
Rediet Girmay <redietpro@gmail.com>
2025-05-30 07:03:48 -0700

    Attachment Update

---
+++ Backend/Model/Dto/Attachments/AttachmentUploadDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectManagementSystem1.Model.Dto.Attachments
+{
+    public class AttachmentUploadDto
+    {
+        [Required]
+        public IFormFile File { get; set; }
+
+        [Required]
+        public AttachmentCategory Category { get; set; }
+
+        [Required]
+        public Guid EntityId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string EntityType { get; set; }
+
+        public int? ProjectTaskId { get; set; }
+    }
+}
---
+++ Backend/Services/AttachmentService/IAttachmentService.cs
@@ -0,0 +1,12 @@
+﻿using ProjectManagementSystem1.Model.Dto.Attachments;
+
+namespace ProjectManagementSystem1.Services.AttachmentService
+{
+    public interface IAttachmentService
+    {
+        Task<Attachment> UploadAttachmentAsync(AttachmentUploadDto uploadDto, string uploadedByUserId);
+        Task<Attachment> GetAttachmentByIdAsync(Guid id);
+        Task<IEnumerable<Attachment>> GetAttachmentByEntityAsync(string entityType, Guid entityId);
+        Task SoftDeleteAttachmentAsync(Guid id);
+    }
+}
