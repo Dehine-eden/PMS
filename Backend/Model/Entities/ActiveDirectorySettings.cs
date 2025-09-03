689086450f4c0eff88ba038b5b278c49e444a72e
Dehine Mulatu Taffesse <119399020+Dehine-eden@users.noreply.github.com>
2025-09-03 13:23:52 +0300

    Create ActiveDirectorySettings.cs

---
+++ Backend/Model/Entities/ActiveDirectorySettings.cs
@@ -0,0 +1,10 @@
+namespace ProjectManagementSystem1.Model.Entities
+{
+    public class ActiveDirectorySettings
+    {
+        public string Domain { get; set; }
+        public string Container { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
